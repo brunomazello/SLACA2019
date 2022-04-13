f7f32c6668553c47fbbae6a2c4fc7e1161c27140
Bruno <bruno.rmazello@gmail.com>
2022-04-13 10:18:06 -0300

    feat(respostas): - Adicionado mostrar respostas.

--- js/script.js
+++ js/script.js
@@ -3,15 +3,20 @@ const criarPost = document.querySelector('.botao_criar')
 const campoDiscussoes = document.querySelector('.discussoes__campos')
 const verMais = document.querySelector('.ver__mais')
 const textoCompleto = document.querySelector('.continuar__texto')
+const maisComentarios = document.querySelector('#respostas')
+const boxRespostas = document.querySelector('.respostas__perguntas')
 
-console.log(verMais)
-console.log(textoCompleto)
+console.log(maisComentarios)
+console.log(boxRespostas)
+
+maisComentarios.addEventListener('click', e => {
+    boxRespostas.classList.remove('d-none')
+})
 
 verMais.addEventListener('click', e => {
     textoCompleto.classList.remove('d-none')
     verMais.innerHTML = "";
 })
-
 
 criarPost.addEventListener('click', e => {
     post.classList.add('d-none')
